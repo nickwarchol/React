6120dd945a8b70ed1045e3a939a6ec421ce33a69
nickwarchol <77034574+nickwarchol@users.noreply.github.com>
2021-03-23 04:19:52 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/footer';
+import NavBar from './components/navBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+        <h1>Hello World!</h1>
+        <Footer></Footer>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ navBar.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                NavBar will be here!!
+            </div>
+         );
+    }
+}
+
+ 
+export default NavBar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//imrc
+//cc
